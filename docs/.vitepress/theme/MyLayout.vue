80741f267a14ee42a7dc8b075a21b11093db3d3d
WizardsBowl <125746317+WizardsBowl@users.noreply.github.com>
2026-03-08 19:34:07 +0800

    site: 将版权声明组件独立出来

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 
-import { computed } from "vue";
 import DefaultTheme from 'vitepress/theme'
 import PostCover from "./components/PostCover.vue";
-import { useData } from "vitepress";
-import { getUrlByPageData } from '../shared';
+import CopyrightBlock from "./components/CopyrightBlock.vue";
 
 const { Layout } = DefaultTheme
-const VPData = useData();
-const pageUrl = computed(() => getUrlByPageData(VPData.page.value));
 
 </script>
 
@@ -18,18 +14,11 @@ const pageUrl = computed(() => getUrlByPageData(VPData.page.value));
       <PostCover />
     </template>
     <template #doc-after>
-      <div class="copyright-block tip custom-block">
-        <p class="custom-block-title custom-block-title-default">版权声明</p>
-        <p>作者主页：<a href="https://github.com/WizardsBowl" target="_blank">WizardsBowl@GitHub</a></p>
-        <p>文章链接：<a :href="pageUrl" target="_blank">{{ pageUrl }}</a></p>
-        <p>许可协议：本站文章除特别声明外，均采用<a href="https://creativecommons.org/licenses/by/4.0" target="_blank">CC BY 4.0国际许可协议</a>，转载时请注明作者和出处。</p>
-      </div>
+      <CopyrightBlock />
     </template>
   </Layout>
 </template>
 
 <style>
-.copyright-block {
-  margin: 16px 0;
-}
+
 </style>
---
+++ docs/.vitepress/theme/components/CopyrightBlock.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+import { computed } from "vue";
+import { useData } from "vitepress";
+import { getUrlByPageData } from '../../shared';
+
+const VPData = useData();
+const pageUrl = computed(() => getUrlByPageData(VPData.page.value));
+
+</script>
+
+<template>
+    <div class="copyright-block tip custom-block">
+        <p class="custom-block-title custom-block-title-default">版权声明</p>
+        <p>作者主页：<a href="https://github.com/WizardsBowl" target="_blank">WizardsBowl@GitHub</a></p>
+        <p>文章链接：<a :href="pageUrl" target="_blank">{{ pageUrl }}</a></p>
+        <p>许可协议：本站文章除特别声明外，均采用<a href="https://creativecommons.org/licenses/by/4.0" target="_blank">CC BY 4.0国际许可协议</a>，转载时请注明作者和出处。</p>
+    </div>
+</template>
+
+<style>
+.copyright-block {
+  margin: 16px 0;
+}
+</style>
